42df258e9a58e2ec46cdac395456a7929fec65b8
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-12-10 00:02:32 +0100

    Create morse_code_decipherment.py
    Basic morse code encryption/decryption method with two dictionaries.

---
+++ morse_code_decipherment.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# morse_code_decipherment.py
+
+ALPHABET_TO_MORSE = {'A': '*-', 'B': '-***', 'C': '-*-*', 'D': '-**', 'E': '*', 'F': '**-*', 
+            'G': '--*', 'H': '****', 'I': '**', 'J': '*----', 'K': '-*-', 'L': '*-**', 'M': '--',
+            'N': '-*', 'O': '---', 'P': '*--*', 'Q': '--*-', 'R': '*-*', 'S': '***', 'T': '-',
+            'U': '**-', 'V': '***-', 'W': '*--', 'X': '-**-', 'Y': '-*--', 'Z': '--**', '1': '*----',
+            '2': '**---', '3': '***--', '4': '****-', '5': '*****', '6': '-****', '7': '--***', '8': '---**',
+            '9': '----*', '0': '-----', '.': '*-*-*-', ',': '--**--', ':': '---***', ';': '-*-*-*', '?': '**--**',
+            '!': '-*-*--', '-': '-****-'}
+
+MORSE_TO_ALPHABET = {'*-': 'A', '-***': 'B', '-*-*': 'C', '-**': 'D', '*': 'E', '**-*': 'F', 
+            '--*': 'G', '****': 'H', '**': 'I', '*----': 'J', '-*-': 'K', '*-**': 'L', '--': 'M',
+            '-*': 'N', '---': 'O', '*--*': 'P', '--*-': 'Q', '*-*': 'R', '***': 'S', '-': 'T',
+            '**-': 'U', '***-': 'V', '*--': 'W', '-**-': 'X', '-*--': 'Y', '--**': 'Z', '*----': '1',
+            '**---': '2', '***--': '3', '****-': '4', '*****': '5', '-****': '6', '--***': '7', '---**': '8',
+            '----*': '9', '-----': '0', '*-*-*-': '.', '--**--': ',', '---***': ':', '-*-*-*': ';', '**--**': '?',
+            '-*-*--': '!', '-****-': '-'}
+
+def alphabet_to_morse_code(sentence):
+    morse_code = ""
+    words = sentence.split()
+    for word in words:
+        for letter in word:
+            for key in ALPHABET_TO_MORSE:
+                if letter.capitalize() == key:
+                    morse_code += ALPHABET_TO_MORSE[key]
+            morse_code += " "
+    return morse_code[0:-1]
+
+def morse_to_alphabet(morse_to_sentence):
+    translated = ""
+    words = morse_to_sentence.split()
+    for word in words:
+        for key in MORSE_TO_ALPHABET:
+            if key == word:
+                translated += MORSE_TO_ALPHABET[key]
+        translated += " "
+    return translated
+
+if __name__ == '__main__':
+    phrase = 'help me, sos!'
+    sentence_to_morse = alphabet_to_morse_code(phrase)
+    print(sentence_to_morse)
+    morse_to_sentence = morse_to_alphabet(sentence_to_morse)
+    print(morse_to_sentence)
